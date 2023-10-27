1c898b2273ba858472416e11b58623c445090d13
mohamed amin <mohamedamin0az@gmail.com>
2023-10-27 04:21:11 +0200

    search feature

--- src/pages/user/home.jsx
+++ src/pages/user/home.jsx
@@ -1,16 +1,18 @@
 import noBooks from "../../assets/noBooks.jpeg";
 import BookCard from "../../components/user/bookCard";
-import { useEffect, useState } from "react";
-import styles from "../../style/bookCard.module.css"
+import { useEffect, useState, useRef } from "react";
+import { Button } from "@mui/material";
+import styles from "../../style/bookCard.module.css";
 
 function Home() {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [searchResult, setSearchResult] = useState(null);
+  const inputField = useRef();
   useEffect(() => {
     (async function () {
       try {
-        const res = await fetch(`http://localhost:8000/books`, {
+        const res = await fetch(`http://localhost:8000/available-books`, {
           credentials: "include",
         });
         const data = await res.json();
@@ -23,6 +25,18 @@ function Home() {
     })();
   }, []);
 
+  async function handelSearch(keyWord) {
+    try {
+      const res = await fetch(`http://localhost:8000/search/${keyWord}`, {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const data = await res.json();
+      setSearchResult(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   if (isLoading) {
     return <div></div>;
   }
@@ -31,8 +45,24 @@ function Home() {
       <div className={styles.container}>
         {books.length > 0 ? (
           <>
+            <div className={styles.search}>
+              <input
+                placeholder="Search ....."
+                ref={inputField}
+                onChange={(e) => {
+                  if (e.target.value.length > 0) {
+                    handelSearch(e.target.value);
+                  } else {
+                    setSearchResult(null);
+                  }
+                }}
+              />
+              <Button variant="contained" size="small" disabled>
+                search
+              </Button>
+            </div>
             <h2>available books</h2>
-            <BookCard books={books}></BookCard>
+            <BookCard books={searchResult ? searchResult : books}></BookCard>
           </>
         ) : (
           <>
